5f4cdaaeb32478ab9ea35f7abd598713f95b6990
zeis974 <38920781+zeis974@users.noreply.github.com>
2023-01-11 14:47:57 +0400

    Add .env.example

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '24bc94a7e0331a770e04ebc786ffb623'),
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
